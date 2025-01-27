fb11fdcb97023d77b7626c3d5a5d1c5651db28f3
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-01-27 18:46:22 +0100

    added delimiter func and test

---
+++ src/delimiter.py
@@ -0,0 +1,24 @@
+from textnode import *
+
+
+def split_nodes_delimiter(old_nodes,delimiter,text_type):
+    new_nodes = []
+    first = None
+    end = None
+    for node in old_nodes :
+        for i in range(0,len(node.text)):
+            if node.text[i] == delimiter and first == None :
+                first = i 
+            if node.text[i] == delimiter and first >= 0:
+                end = i 
+        new_nodes.append(TextNode(node.text[:first],TextType.TEXT))
+        new_nodes.append(TextNode(node.text[first + 1:end],text_type))
+        new_nodes.append(TextNode(node.text[end + 1:],TextType.TEXT))
+    
+    if end is not None:
+        print(f"Delimiter found from index {first} to {end}")
+    else:
+        print("Delimiter not found")
+        
+    return new_nodes
+        
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-
+from delimiter import split_nodes_delimiter
+from enum import Enum
 from textnode import TextNode, TextType
 
 
@@ -21,6 +22,14 @@ class TestTextNode(unittest.TestCase):
         test = TextNode("This is a link", TextType.LINK, "https://www.google.com")
         self.assertEqual(test.text_node_to_html_node(),'<a href="https://www.google.com">This is a link</a>')
     
+    def test_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes,[
+    TextNode("This is text with a ", TextType.TEXT),
+    TextNode("code block", TextType.CODE),
+    TextNode(" word", TextType.TEXT),
+])
 
 if __name__ == "__main__":
     unittest.main()
